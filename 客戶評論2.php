e69beaefda0a014b18a5e99386587640afba51f2
xi455 <79513741+xi455@users.noreply.github.com>
2021-02-25 21:24:02 +0800

    Add files via upload

---
+++ 客戶評論2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+        <link rel="stylesheet" href="0212.css">
+       
+</head>
+<body>
+    <div style=" position: relative; margin-top: 30px; width: 60%;margin-left:auto; margin-right: auto; height: 650px; background-color: bisque;" >
+        <div style="border: 1px;  padding-left: 30px; padding-top: 10px;">
+            <h2>
+                紫雲農場 - 觀音山有機綠竹筍點餐
+            </h2>
+        </div>
+        <div style="margin-top: 20px; background-color: darkgrey; ">
+            <table style="background-color: darkgrey;">
+            <tr>
+                <td align="center" id="a"><a href="index.html" style="color:black; text-decoration:none;">首頁</a></td>
+                <td id="a" align="center"><a href="最新推出.html" style="color:black; text-decoration:none;">最新推出</a></td>
+                <td id="a" align="center"><a href="菜單.html" style="color:black; text-decoration:none;">菜單</a></td>
+                <td id="a" align="center" style="background-color: coral;"><a href="客戶評論.html" style="color:black; text-decoration:none;">店長推薦產品</a></td>
+                
+            </tr>
+            </table>
+        </div>
+        
+       
+
+
+        <form action="客戶評論2.php" method="POST" id="form">
+           
+            
+                    
+                    
+                    
+                   
+
+
+
+
+         
+            </span>
+            
+            
+        </form>
+
+
+
+<img src="紫雲農場竹筍訂購頁面.jpg"  style="margin-left: 5%; margin-top: 90px; width: 200px; height: 200px;">
+
+        <br>
+        <img src="12.png" style="width: 250px; height: 100px; margin-top: 20px; margin-left: 2%;">
+        
+        
+       
+        
+       
+        
+       
+        
+
+    </div>
+</body>
+</html>
